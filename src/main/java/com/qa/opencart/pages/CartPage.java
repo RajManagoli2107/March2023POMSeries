a2fe1e4047533e2d249a2338b8aedde74506270d
Vedant Managoli <vedant@vedants-mbp.home>
2023-08-02 23:55:39 +0100

    updated the cartpage.java as per RajTl's review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,12 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String cartPage ="Testing";
-
+		By product = By.id("Product");
+	
+	}
+	public void clickproduct() {
+		System.out.println("product");
 	}
 
 }
